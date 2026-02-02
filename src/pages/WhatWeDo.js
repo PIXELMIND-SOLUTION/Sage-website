cf048e1a6420fae742c571ebd3e8b20210c77cc8
vijaynimmakayala9 <vijay.pixelmindsolutions@gmail.com>
2026-02-02 19:20:18 +0530

    images location

--- src/pages/WhatWeDo.js
+++ src/pages/WhatWeDo.js
@@ -60,7 +60,7 @@ const WhatWeDo = () => {
                 className="relative min-h-[85vh] flex items-center bg-cover bg-center"
                 style={{
                     backgroundImage:
-                        "url('src/Assets/images/whatwedo/whatwedo.png')",
+                        "url('/src/Assets/images/whatwedo/whatwedo.png')",
                 }}
             >
                 <div className="absolute inset-0 bg-gradient-to-br from-black/80 via-black/60 to-indigo-900/70" />
